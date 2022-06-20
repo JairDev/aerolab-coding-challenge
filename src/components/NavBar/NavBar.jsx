eb51eb7d6c7967d4956441bcc87e26103b9fe2a5
JairDev <devalfredomoscoso@gmail.com>
2022-06-20 14:37:28 -0400

    Added landing section styles

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,23 @@
-import "./NavBar.css"
+import aerolabLogo from "../../assets/icons/aerolab-logo.svg";
+import dropIcon from "../../assets/icons/drop-icon.svg";
+import "./NavBar.css";
 
 function NavBar() {
   return (
-    <header>
+    <header className="App-header">
       <nav className="App-nav">
-        <div className="App-nav-container-logo">logo</div>
+        <div className="App-nav-container-logo">
+          <img src={aerolabLogo} />
+        </div>
         <div className="App-nav-container-action">
           <div className="App-nav-container-aerocoins">
-            <div className="aerocoins-logo">logo</div>
+            <div className="aerocoins-logo">
+              <img src={aerolabLogo} />
+            </div>
             <div className="aerocoins-points">10000</div>
-            <div className="aerocoins-dropdown-icon">drop</div>
+            <div className="aerocoins-dropdown-icon">
+              <img src={dropIcon} />
+            </div>
           </div>
         </div>
       </nav>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,33 @@
+import heroImage from "../../assets/img/hero-image.png"
+import walkCard1 from "../../assets/img/walk-card1.png"
+import walkCard2 from "../../assets/img/walk-card2.png"
+import walkCard3 from "../../assets/img/walk-card3.png"
+import "./Home.css"
+
 function Home() {
   return (
     <main>
-      <section className="landing"></section>
+      <section className="landing">
+        <div className="landing-cta-container">
+          <div className="proposal-container">
+            <span className="proposal-label">Explore The</span>
+            <h1 className="proposal-title">Tech Zone</h1>
+            <h2 className="proposal-subtitle">
+              Here you’ll be able to exchange all of your hard-earned Aeropoints
+              and exchange them for cool tech.
+            </h2>
+          </div>
+          <div className="cta-button-container">
+            button
+          </div>
+        </div>
+        <div className="landing-illustration-container">
+          <div className="illustration-container">
+            <img src={heroImage} alt=""/>
+          </div>
+        </div>
+      </section>
+     
     </main>
   );
 }
