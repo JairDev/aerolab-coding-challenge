c8643f9d944d003160c6581f49750754558bd32b
JairDev <devalfredomoscoso@gmail.com>
2022-06-20 15:35:53 -0400

    Added styles walkthrough section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,9 @@
-import heroImage from "../../assets/img/hero-image.png"
-import walkCard1 from "../../assets/img/walk-card1.png"
-import walkCard2 from "../../assets/img/walk-card2.png"
-import walkCard3 from "../../assets/img/walk-card3.png"
-import "./Home.css"
+import heroImage from "../../assets/img/hero-image.png";
+import walkCard1 from "../../assets/img/walk-card1.png";
+import walkCard2 from "../../assets/img/walk-card2.png";
+import walkCard3 from "../../assets/img/walk-card3.png";
+import walkCardLogo from "../../assets/icons/walk-card-logo.svg";
+import "./Home.css";
 
 function Home() {
   return (
@@ -17,17 +18,70 @@ function Home() {
               and exchange them for cool tech.
             </h2>
           </div>
-          <div className="cta-button-container">
-            button
-          </div>
+          <div className="cta-button-container">VIEW ALL PRODUCTS</div>
         </div>
         <div className="landing-illustration-container">
           <div className="illustration-container">
-            <img src={heroImage} alt=""/>
+            <img src={heroImage} alt="" />
           </div>
         </div>
       </section>
-     
+      <section className="walkthrough-section">
+        <div className="container-walkthrough-cards">
+          <div className="walkthrough-card">
+            <div className="walkthrough-card-header">
+              <img src={walkCard1} />
+            </div>
+            <div className="walkthrough-card-footer">
+              <div className="walkthrough-card-footer-title">
+                <div className="walkthrough-card-footer-contain-logo">
+                  <img src={walkCardLogo} />
+                </div>
+                <p className="walkthrough-card-title">1—browse</p>
+              </div>
+              <div className="walkthrough-card-body">
+                <p>
+                  Browse our tech catalog with more than 20 top tech products
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="walkthrough-card">
+            <div className="walkthrough-card-header">
+              <img src={walkCard2} />
+            </div>
+            <div className="walkthrough-card-footer">
+              <div className="walkthrough-card-footer-title">
+                <div className="walkthrough-card-footer-contain-logo">
+                  <img src={walkCardLogo} />
+                </div>
+                <p className="walkthrough-card-title">2—choose</p>
+              </div>
+              <div className="walkthrough-card-body">
+                <p>Exchange your hard-earned AeroPoints for a cool tech item</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="walkthrough-card">
+            <div className="walkthrough-card-header">
+              <img src={walkCard3} />
+            </div>
+            <div className="walkthrough-card-footer">
+              <div className="walkthrough-card-footer-title">
+                <div className="walkthrough-card-footer-contain-logo">
+                  <img src={walkCardLogo} />
+                </div>
+                <p className="walkthrough-card-title">3—enjoy</p>
+              </div>
+              <div className="walkthrough-card-body">
+                <p>All done We’ll take care of delivery of your tech item!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
