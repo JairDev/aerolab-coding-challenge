b11ed0798a294e4331c597eb2da3d047ad98322b
JairDev <devalfredomoscoso@gmail.com>
2022-06-21 13:05:13 -0400

    Added styles product section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -82,6 +82,64 @@ function Home() {
           </div>
         </div>
       </section>
+
+      <section className="product-section">
+        <div className="product-section-container">
+          <div className="product-section-header">
+            <div className="product-section-header-title">
+              <h2>Tech Products</h2>
+            </div>
+            <div className="product-section-header-filter">
+              <div className="product-section-header-filter-category">
+                <form className="form-filter-category">
+                  <label htmlFor="filter">Filter by</label>
+                  <select className="select-filter-category" name="filter" id="">
+                    <option value="">All products</option>
+                    <option value="">Gaming</option>
+                    <option value="">Audio</option>
+                    <option value="">Smart Home</option>
+                    <option value="">Monitors & Tv</option>
+                  </select>
+                </form>
+              </div>
+
+              <div className="product-section-header-filter-price">
+                <span>Sort by</span>
+                <div className="filter-price-options-container">
+                  <div className="filter-price-option">
+                    <p>Most Recent</p>
+                  </div>
+                  <div className="filter-price-option">
+                    <p>Lowest Price</p>
+                  </div>
+                  <div className="filter-price-option">
+                    <p>Highest Price</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="container-products">
+            <div className="product-card">
+              <div className="product-card-header">
+                <img src={walkCard3} />
+              </div>
+              <div className="product-card-footer">
+                <div className="product-card-footer-title">
+                  <p className="product-card-title">Audifonos</p>
+                  <span className="product-card-subtitle">Home</span>
+                </div>
+                <div className="product-card-action">
+                  <form action="">
+                    <button className="button-redeem">Redeem</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
