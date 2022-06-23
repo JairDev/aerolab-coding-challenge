23daf8f2b191caf7de1486bdef4a53b12a48c8fd
JairDev <devalfredomoscoso@gmail.com>
2022-06-23 14:57:53 -0400

    Create function to redeem points

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -2,18 +2,23 @@ import { useContext, useReducer } from "react";
 import walkCard3 from "../../assets/img/walk-card3.png";
 import { AerolabContextData } from "../../context";
 import { dataService } from "../../services/data.service";
-// import { createHeader } from "../NavBar/NavBar";
 import "./ProductCard.css";
 
 const ULR_PRODUCT_REDEEM = "https://coding-challenge-api.aerolab.co/redeem";
 
-function ProductCard({ id, imgSrc, productName, productCategory, productCost }) {
-
+function ProductCard({
+  id,
+  imgSrc,
+  productName,
+  productCategory,
+  productCost,
+}) {
+  const {dispatch} = useReducer(AerolabContextData)
   const handleClick = (e) => {
-    console.log(e.target.dataset.idproduct)
+    console.log(e.target.dataset.idproduct);
 
-    e.preventDefault()
-  }
+    e.preventDefault();
+  };
   return (
     <div className="product-card">
       <div className="product-card-header">
@@ -26,7 +31,12 @@ function ProductCard({ id, imgSrc, productName, productCategory, productCost }) 
         </div>
         <div className="product-card-action">
           <form action="">
-            <button onClick={handleClick} value="20000" data-idproduct={id} className="button-redeem">
+            <button
+              onClick={handleClick}
+              value="20000"
+              data-idproduct={id}
+              className="button-redeem"
+            >
               Redeem for {productCost}
             </button>
           </form>
