43d306e418f2e591182d3026e60dee0ad55ee8e1
JairDev <devalfredomoscoso@gmail.com>
2022-06-23 14:07:41 -0400

    Create reducer for handle actions and state

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -4,3 +4,18 @@ export const initialState = {
   productsData: [],
   redeemMessage: {}
 };
+
+export const reducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case "receiveUserData":
+      return { ...state, userData: payload };
+    case "receiveProductsDate": 
+    return {...state, productsData: payload}
+    case "addPoints": 
+    return {...state, points: payload}
+    case "redeemPoints":
+      return {...state, redeemMessage: payload}
+    default:
+      return state;
+  }
+}
