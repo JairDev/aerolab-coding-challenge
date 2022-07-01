5bdfd2a220be0bbe8d3b54fafe635ced5aa1b608
JairDev <devalfredomoscoso@gmail.com>
2022-07-01 13:18:38 -0400

    Added routes with react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
 import { useReducer } from "react";
-import "./App.css";
-import { AerolabContextData } from "./context";
-import Home from "./pages/Home/Home";
-import Layout from "./pages/Layout/Layout";
+import { Routes, Route, Link } from "react-router-dom";
 import { initialState, reducer } from "./reducer";
+import { AerolabContextData } from "./context";
+import Layout from "./pages/Layout/Layout";
+import Home from "./pages/Home/Home";
+import RedeemHistory from "./pages/RedeemHistory/RedeemHistory";
+import "./App.css";
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState)
-  
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
-      <AerolabContextData.Provider value={{state, dispatch}}>
-        <Layout>
-          <Home />
-        </Layout>
+      <AerolabContextData.Provider value={{ state, dispatch }}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Layout>
+                <Home />
+              </Layout>
+            }
+          />
+          <Route
+            path="redeem-history"
+            element={
+              <Layout>
+                {" "}
+                <RedeemHistory />
+              </Layout>
+            }
+          />
+        </Routes>
       </AerolabContextData.Provider>
     </div>
   );
--- src/pages/RedeemHistory/RedeemHistory.jsx
+++ src/pages/RedeemHistory/RedeemHistory.jsx
@@ -1,7 +1,14 @@
+import { useContext } from "react"
+import { AerolabContextData } from "../../context"
+
 function RedeemHistory() {
+  const {state} = useContext(AerolabContextData)
+  console.log(state)
   return(
     <div className="content-redeem-history">
-
+      Redeem
     </div>
   )
 }
+
+export default RedeemHistory
